58dd242bf146ba0878d8b9d6bcd53a7ec380b417
ANIRBAN CHAKRABORTY <discoveranirban@gmail.com>
2020-07-26 12:12:46 +0530

    associated streams with userID

--- Client/src/actions/index.js
+++ Client/src/actions/index.js
@@ -22,14 +22,19 @@ export const changeStat=(val,id)=>{
 }
 
 export const createStream = (formValues)=>{
-    return async (dispatch)=>{
-        const response=await streams.post('/streams',formValues)
+    return async (dispatch,getState)=>{         
+        const { userId }=getState().logStat;
+        const response=await streams.post('/streams',{...formValues,userId})
         dispatch({
             type:'CREATE_STREAM',
             payload:response.data
         })
     }
 }
+
+// when action creator return function it gets called by two parameters, dispatch function and getState function
+// which gives us access to all of our state. so we pull out UserID from there.
+// to add userId to formValues object we use ES6 syntax. We spread out all the formValues key value pairs and add userId to it
 
 export const fetchStreams = ()=>{
     return async (dispatch)=>{
--- Client/src/components/streams/StreamList.js
+++ Client/src/components/streams/StreamList.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { fetchStreams } from '../../actions/'
+import { Link } from 'react-router-dom'
 
 class StreamList extends React.Component {
 
@@ -8,17 +9,71 @@ class StreamList extends React.Component {
         this.props.fetchStreams();
     }
 
+
+    renderAdmin(stream){
+        if(stream.userId===this.props.currentUserID){
+            return (
+                <div className="right floated content">
+                    <button className="ui button primary ">Edit</button>
+                    <button className="ui button negative ">Delete</button>
+                </div>
+            )
+        }
+    }
+
+    renderList(){
+        return this.props.streams.map(stream=>{
+            return (
+                <div className="item" key={stream.id}>
+                    {this.renderAdmin(stream)}
+                    <i className="large middle aligned icon camera"/>
+                    <div className="content">
+                        {stream.title}
+                        <div className="description">
+                            {stream.description}
+                        </div>
+                    </div>
+                </div>
+            )
+            
+        })
+    }
+
+    renderCreate(){
+        if(this.props.isSignedIn){
+            return (
+                <div style={{textAlign:'right'}}>
+                    <Link to="/streams/new" className="ui button primary">
+                    Create Stream
+                    </Link>
+                </div>
+            )
+        }
+    }
+
     render(){
         return (
             <div>
-                StreamList
+                <h2>Streams:</h2>
+                <div className="ui celled list">
+                    {this.renderList()}
+                </div>
+                {this.renderCreate()}
             </div>
         )
     }
 }
 
+const mapStateToProps=(state)=>{
+    return { streams: Object.values(state.streams), 
+             currentUserID:state.logStat.userId,
+             isSignedIn:state.logStat.isSignedIn               
+    }                                                
+}                                                     
 
-export default connect(null,{ fetchStreams:fetchStreams })(StreamList); 
+// stream is stored as an object in redux state for ease of access but here to render we convert it to array so that we can use map function on it
+
+export default connect(mapStateToProps,{ fetchStreams:fetchStreams })(StreamList); 
 
 //connect takes 2 parameters, 1st one is mapStateToProps which maps our redux states to props of this component 
 // second parameter is the ACTION CREATORS as an OBJECT and makes them available in props of this component
